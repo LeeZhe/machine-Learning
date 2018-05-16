38a88499cca077b126ae84f782b164fcee1f74b1
LeeZhe <e_zhe@sina.com>
2018-05-17 01:04:19 +0800

    no message

--- smartcab-master/smartcab/agent.py
+++ smartcab-master/smartcab/agent.py
@@ -188,7 +188,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay=0.0001,log_metrics=True,display=False,optimized=True)
+    sim = Simulator(env,update_delay=0.0001,log_metrics=True,optimized=True)
     
     ##############
     # Run the simulator
